1e2a099eb1efb1972b8fa59a803e6ef944aa1c79
Ciahaka <wilddiver.by@gmail.com>
2022-12-07 23:35:51 +0300

    add new component to Counter

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,33 +1,43 @@
 import React from 'react';
-import {Button} from "./Button";
-import classes from "./Counter.module.css"
+import {Button} from './Button';
+import classes from './Counter.module.css'
+import {ChangeMaxValue} from './ChangeMaxValue';
 
 export type propsType = {
-    number: number
-    callBackHandlerReset: () => void
-    callBackHandlerIncrement: () => void
-    startValue: number
-    maxValue: number
+  number: number
+  callBackHandlerReset: () => void
+  callBackHandlerIncrement: () => void
+  startValue: number
+  maxValue: number
+
 }
 
 export const Counter = (props: propsType) => {
-    return (
-        <div className={classes.count}>
-            <div className={classes.number}>
-                <span className={props.number === props.maxValue ? classes.numberRed : ""}>{props.number} </span>
-            </div>
-            <div className={classes.buttons}>
-                <Button name='inc'
-                        callBack={props.callBackHandlerIncrement}
-                    disabled={props.number === props.maxValue}
 
-                />
-                <Button name='reset'
-                        callBack={props.callBackHandlerReset}
-                        disabled={props.number === props.startValue}
-                />
-            </div>
-        </div>
-    )
+  return (
+    <div className={classes.count}>
+      <div className={classes.number}>
+        <ChangeMaxValue
+          number={props.number}
+          maxValue={props.maxValue}
+          activationRenderingCallback={()=>{}}
+
+
+        />
+      </div>
+      <div className={classes.buttons}>
+        <Button name="inc"
+                callBack={props.callBackHandlerIncrement}
+                disabled={props.number === props.maxValue}
+
+
+        />
+        <Button name="reset"
+                callBack={props.callBackHandlerReset}
+                disabled={props.number === props.startValue}
+        />
+      </div>
+    </div>
+  )
 
 }
